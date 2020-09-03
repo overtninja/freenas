8bb5df51dbd66b4feedda43e23f3f7512d8d48d0
themylogin <themylogin@gmail.com>
2020-09-03 18:43:02 +0200

    Improve VolumeVersionAlertSource
    (cherry picked from commit 7ec92177ba3526b29380602fd6a023da6180128c)

--- src/middlewared/middlewared/alert/source/volume_version.py
+++ src/middlewared/middlewared/alert/source/volume_version.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-import subprocess
 
 from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, ThreadedAlertSource
 from middlewared.alert.schedule import IntervalSchedule
@@ -10,20 +9,8 @@ class VolumeVersionAlertClass(AlertClass):
     level = AlertLevel.WARNING
     title = "New Feature Flags Are Available for Pool"
     text = (
-        "New feature flags are available for volume %s. Refer "
-        "to the \"Upgrading a ZFS Pool\" subsection in the "
-        "User Guide \"Installing and Upgrading\" chapter "
-        "and \"Upgrading\" section for more instructions."
-    )
-
-
-class ZfsVersionOutOfDateAlertClass(AlertClass):
-    category = AlertCategory.STORAGE
-    level = AlertLevel.WARNING
-    title = "ZFS Filesystem Version Is Out of Date"
-    text = (
-        "ZFS filesystem version is out of date. Please consider upgrading it. See <a href=\""
-        "https://www.ixsystems.com/documentation/freenas/11.2/install.html#upgrading-a-zfs-pool\">"
+        "New ZFS version or feature flags are available for pool %s. Please see <a href=\""
+        "https://www.truenas.com/docs/hub/tasks/advanced/upgrading-pool/\">"
         "Upgrading a ZFS Pool</a> for details."
     )
 
@@ -39,16 +26,5 @@ class VolumeVersionAlertSource(ThreadedAlertSource):
                     VolumeVersionAlertClass,
                     pool["name"],
                 ))
-
-        proc = subprocess.Popen(
-            "zfs upgrade | grep FILESYSTEM",
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            encoding="utf8",
-        )
-        output = proc.communicate()[0].strip(" ").strip("\n")
-        if output:
-            alerts.append(Alert(ZfsVersionOutOfDateAlertClass))
 
         return alerts
